bcdf7eb40a50f8edde87df76c92963b26ee2a93e
Alex Harper <alex.harepr@gmx.com>
2014-05-22 19:47:02 +0100

    First Commit

---
+++ Pomodoro/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Pomodoro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn_go = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.txt_interval = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_go
+            // 
+            this.btn_go.Location = new System.Drawing.Point(12, 38);
+            this.btn_go.Name = "btn_go";
+            this.btn_go.Size = new System.Drawing.Size(183, 29);
+            this.btn_go.TabIndex = 0;
+            this.btn_go.Text = "Start";
+            this.btn_go.UseVisualStyleBackColor = true;
+            this.btn_go.Click += new System.EventHandler(this.btn_go_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // txt_interval
+            // 
+            this.txt_interval.Location = new System.Drawing.Point(98, 12);
+            this.txt_interval.Name = "txt_interval";
+            this.txt_interval.Size = new System.Drawing.Size(97, 20);
+            this.txt_interval.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Interval";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(207, 77);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_interval);
+            this.Controls.Add(this.btn_go);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Pomodoro";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_go;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox txt_interval;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Pomodoro/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Shell;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+
+namespace Pomodoro
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool FlashWindowEx(ref FLASHWINFO pwfi);
+
+        int intervalTime = 0;
+        TimeSpan minutes;
+        DateTime start; 
+        DateTime endTime;
+
+        public Form1()
+        {
+            InitializeComponent();
+            txt_interval.Text = "25";
+        }
+
+
+        //
+        //
+        private void btn_go_Click(object sender, EventArgs e)
+        {
+            intervalTime = Convert.ToInt16(txt_interval.Text);
+            minutes = TimeSpan.FromMinutes(intervalTime); 
+            start = DateTime.UtcNow; // Use UtcNow instead of Now
+            endTime = start.AddMinutes(intervalTime); //endTime is a member, not a local variable
+
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            TimeSpan remainingTime = endTime - DateTime.UtcNow;
+            if (remainingTime < TimeSpan.Zero)
+            {
+                this.Text = "Pomodoro";
+                label1.Text = "Done!";
+                timer1.Enabled = false;
+                FlashWindowEx(this);
+            }
+            else
+            {
+                this.Text = remainingTime.ToString();
+            }
+        }
+
+        //Flash both the window caption and taskbar button.
+        //This is equivalent to setting the FLASHW_CAPTION | FLASHW_TRAY flags. 
+        public const UInt32 FLASHW_ALL = 3;
+
+        // Flash continuously until the window comes to the foreground. 
+        public const UInt32 FLASHW_TIMERNOFG = 12;
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct FLASHWINFO
+        {
+            public UInt32 cbSize;
+            public IntPtr hwnd;
+            public UInt32 dwFlags;
+            public UInt32 uCount;
+            public UInt32 dwTimeout;
+        }
+
+        // Do the flashing - this does not involve a raincoat.
+        public static bool FlashWindowEx(Form form)
+        {
+            IntPtr hWnd = form.Handle;
+            FLASHWINFO fInfo = new FLASHWINFO();
+
+            fInfo.cbSize = Convert.ToUInt32(Marshal.SizeOf(fInfo));
+            fInfo.hwnd = hWnd;
+            fInfo.dwFlags = FLASHW_ALL | FLASHW_TIMERNOFG;
+            fInfo.uCount = UInt32.MaxValue;
+            fInfo.dwTimeout = 0;
+
+            return FlashWindowEx(ref fInfo);
+        }
+    }
+}
+
